28e393b9477b3a82d9e45f0fe106e1b0fdb2bdc1
ErickMolnarAlcalde <erickmolnar.alcalde@gmail.com>
2025-11-08 05:31:13 -0300

    apagando time

--- agitask/src/main/java/com/example/agitask/repository/TimeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.agitask.repository;
-
-import com.example.agitask.model.Time;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimeRepository extends JpaRepository<Time, Long> {
-}
