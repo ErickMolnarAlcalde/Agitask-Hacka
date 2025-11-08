687df366da68686d42a91e849a761559be290217
Matheus Reis <matheusdanireis@gmail.com>
2025-11-08 03:28:08 -0300

    Criacao da entidade relatorios

---
+++ agitask/src/main/java/com/example/agitask/model/Relatorios.java
@@ -0,0 +1,48 @@
+package com.example.agitask.model;
+
+import com.example.agitask.dto.RelatoriosRequestDTO;
+import com.example.agitask.dto.RelatoriosResponseDTO;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "Relatorios")
+@Entity(name = "Relatorios")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Relatorios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "projeto_id")
+    private Projeto idProjeto;
+
+    private Long idTarefa;
+
+    @ManyToOne
+    @JoinColumn(name = "responsavel_id")
+    private Usuario idResponsavel;
+
+    public Relatorios(RelatoriosRequestDTO dados) {
+        this.idProjeto = dados.idProjeto();
+        this.idTarefa = dados.idTarefa();
+        this.idResponsavel = dados.idResponsavel();
+    }
+
+    public Relatorios(RelatoriosResponseDTO dados) {
+        this.idProjeto = dados.idProjeto();
+        this.idTarefa = dados.idTarefa();
+        this.idResponsavel = dados.idResponsavel();
+    }
+
+
+}
