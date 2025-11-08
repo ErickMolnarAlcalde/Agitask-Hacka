96bce6a68f228bbcaea6a1028b0a14786cf105cb
MathheusOlveira <thhheusoliveira@gmail.com>
2025-11-08 08:03:01 -0300

    fix entidade e relacao time

--- agitask/src/main/java/com/example/agitask/mapper/UsuarioMapper.java
+++ agitask/src/main/java/com/example/agitask/mapper/UsuarioMapper.java
@@ -10,6 +10,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
