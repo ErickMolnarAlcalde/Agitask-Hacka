8073b98e0a0e1ed62655c4f029d5e5f649993df9
MathheusOlveira <thhheusoliveira@gmail.com>
2025-11-08 08:26:35 -0300

    feat EndPoint Login

---
+++ agitask/src/main/java/com/example/agitask/dto/UsuarioLoginRequestDTO.java
@@ -0,0 +1,22 @@
+package com.example.agitask.dto;
+
+import com.example.agitask.enums.CargoUsuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioLoginRequestDTO {
+
+    @NotBlank
+    private String senha;
+    @NotBlank
+    @Email
+    private String email;
+
+}
